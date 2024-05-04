b107a89d189e976e2af739bc71cbdfd0a5f41d98
atomic <atomic@Ubuntu2.myguest.virtualbox.org>
2024-05-04 20:50:05 +0300

    fix()

--- app/api.php
+++ app/api.php
@@ -3,28 +3,14 @@
 use app\Entities\PrioritetEntity;
 
 require_once 'bootstrap.php';
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-
-
-function debug($str)
-{
-    echo '<pre>';
-    var_dump($str);
-    echo '</pre>';
-    exit;
-}
 
 
 $entityManager = getEntityManager();
 $class = $_GET['act'] ?? '0';
 $method = $_GET['method'] ?? '0';
-$Class = 'app\\controllers\\' . ucfirst($class) . 'Controller';
-$Controller = new $Class($entityManager);
-//$Controller = new \app\controllers\UserController($entityManager);
-//echo $Controller->getPage('createTask');
-echo json_encode($Controller->$method($_REQUEST));
+$class = 'app\\controllers\\' . ucfirst($class) . 'Controller';
+$controller = new $class($entityManager);
+echo json_encode($controller->$method($_REQUEST));
 
 
 
